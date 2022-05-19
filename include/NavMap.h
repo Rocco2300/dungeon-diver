85c983f97b534926266d6eac365c95f0717d0f75
Rocco <grigoreandrey23@gmail.com>
2022-05-19 17:46:12 +0300

    added basic navmap cell storage

--- include/NavMap.h
+++ include/NavMap.h
@@ -1,11 +1,28 @@
 #pragma once
 
+#include <vector>
+
+struct Cell 
+{
+    int _fCost;
+    int _gCost;
+    int _hCost;
+    std::vector<Cell*> neighbours;
+
+    Cell();
+    int fCost();
+    int gCost();
+    int hCost();
+};
+
 class NavMap
 {
 private:
-    
+    std::vector<Cell> cells;
 public:
-    NavMap() = default;
+    NavMap();
+
+    void addNeighbours(int x, int y);
 };
 
 
--- src/NavMap.cpp
+++ src/NavMap.cpp
@@ -1,1 +1,49 @@
 #include "NavMap.h"
+
+#include "Constants.h"
+
+Cell::Cell() 
+{
+    neighbours.resize(4);
+
+    _fCost = 0;
+    _gCost = 0;
+    _hCost = 0;
+}
+
+int Cell::fCost() { return _fCost; }
+int Cell::gCost() { return _gCost; }
+int Cell::hCost() { return _hCost; }
+
+NavMap::NavMap() 
+{
+    for (int i = 0; i < 16; i++)
+    {
+        for (int j = 0; j < 16; j++)
+        {
+            Cell cell;
+            cells.push_back(cell);
+        }
+    }
+
+    for (int i = 0; i < 16; i++)
+    {
+        for (int j = 0; j < 16; j++)
+        {
+            addNeighbours(i, j);
+        }
+    }
+}
+
+void NavMap::addNeighbours(int x, int y)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        if (dirX[i] >= 0 && dirX[i] < 16 && dirY[i] >= 0 && dirY[i] < 16)
+        {
+            int currIdx = y * 16 + x;
+            int neighbourIdx = dirY[i] * 16 + dirX[i];
+            cells[currIdx].neighbours.push_back(&cells[neighbourIdx]);
+        }
+    }
+}
