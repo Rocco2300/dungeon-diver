7c12dfad1db9a529c34f9faffa595269ac12adf6
Rocco <grigoreandrey23@gmail.com>
2022-06-21 22:56:49 +0300

    write implementation

--- include/Grid.h
+++ include/Grid.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cassert>
+
 /** 
  * Custom data class for fixed size 16x16 grid
  * used for custom accessors and logic
@@ -16,9 +18,57 @@ public:
     void alloc();
     void dealloc();
 
-    void fill(int);
-    void clear();
+    void fill(int number = 0);
 
     T& operator[](int index);
     T& operator()(int x, int y);
 };
+
+template <typename T>
+Grid<T>::Grid() : data{nullptr} { }
+
+template <typename T>
+Grid<T>::~Grid()
+{
+    dealloc();
+}
+
+template <typename T>
+void Grid<T>::alloc()
+{
+    data = new T[16 * 16];
+}
+
+template <typename T>
+void Grid<T>::dealloc()
+{
+    if (data)
+        delete[] data;
+}
+
+template <typename T>
+void Grid<T>::fill(int number = 0)
+{
+    assert(data != nullptr);
+
+    for (int i = 0; i < 16 * 16; i++)
+    {
+        data[i] = number;
+    }
+}
+
+template <typename T>
+T& Grid<T>::operator[](int index)
+{
+    assert(index >= 0 && index < 16 * 16);
+
+    return data[index];
+}
+
+template <typename T>
+T& Grid<T>::operator()(int x, int y)
+{
+    assert(x >= 0 && x < 16 * 16 && y >= 0 && y < 16 * 16);
+
+    return data[y * 16 + x];
+}
