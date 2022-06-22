ea49ff0d6875955136b29ff201a4345cadb3467d
Rocco <grigoreandrey23@gmail.com>
2022-06-22 14:49:52 +0300

    basic operator for grid

--- include/Grid.h
+++ include/Grid.h
@@ -2,6 +2,70 @@
 
 #include <cassert>
 
+template <typename Grid>
+class GridIterator
+{
+public:
+    using ValueType = typename Grid::ValueType;
+    using PointerType = ValueType*;
+    using ReferenceType = ValueType&;
+private:
+    PointerType ptr;
+public:
+    GridIterator(PointerType ptr) : ptr{ptr} {}
+
+    GridIterator& operator++()
+    {
+        ptr++;
+        return *this;
+    }
+
+    GridIterator& operator++(int)
+    {
+        auto iterator = *this;
+        ++(*this);
+        return iterator;
+    }
+
+    GridIterator& operator--()
+    {
+        ptr--;
+        return *this;
+    }
+
+    GridIterator& operator--(int)
+    {
+        auto iterator = *this;
+        --(*this);
+        return iterator;
+    }
+
+    ReferenceType operator[](int index)
+    {
+        return *(ptr + index);
+    }
+
+    PointerType operator->()
+    {
+        return ptr;
+    }
+
+    ReferenceType operator*()
+    {
+        return *ptr;
+    }
+
+    bool operator==(const GridIterator& other)
+    {
+        return ptr == other.ptr;
+    }
+
+    bool operator!=(const GridIterator& other)
+    {
+        return ptr != other.ptr;
+    }
+};
+
 /** 
  * Custom data class for fixed size 16x16 grid
  * used for custom accessors and logic
@@ -9,68 +73,65 @@
 template <typename T>
 class Grid
 {
+public:
+    using ValueType = T;
+    using Iterator = GridIterator<Grid<T>>;
 private:
     T* data;
 public:
-    Grid();
-    ~Grid();
+    Grid() : data{nullptr} { }
 
-    void alloc();
-    void dealloc();
+    ~Grid()
+    {
+        dealloc();
+    }
 
-    void fill(T element);
+    void alloc()
+    {
+        data = new T[16 * 16];
+    }
 
-    T& operator[](int index);
-    T& operator()(int x, int y);
+    void dealloc()
+    {
+        if (data != nullptr)
+            delete[] data;
+    }
+
+    void fill(T element)
+    {
+        assert(data != nullptr);
+
+        for (int i = 0; i < 16 * 16; i++)
+        {
+            data[i] = element;
+        }
+    }
+
+    T& operator[](int index)
+    {
+        assert(data != nullptr);
+        assert(index >= 0 && index < 16 * 16);
+
+        return data[index];
+    }
+    
+    T& operator()(int x, int y)
+    {
+        assert(data != nullptr);
+        assert(x >= 0 && x < 16 * 16 && y >= 0 && y < 16 * 16);
+
+        return data[y * 16 + x];
+    }
+
+
+    Iterator begin()
+    {
+        return Iterator(data);
+    }
+
+    Iterator end()
+    {
+        return Iterator(data + (16 * 16));
+    }
 };
 
-template <typename T>
-Grid<T>::Grid() : data{nullptr} { }
-
-template <typename T>
-Grid<T>::~Grid()
-{
-    dealloc();
-}
-
-template <typename T>
-void Grid<T>::alloc()
-{
-    data = new T[16 * 16];
-}
-
-template <typename T>
-void Grid<T>::dealloc()
-{
-    if (data != nullptr)
-        delete[] data;
-}
-
-template <typename T>
-void Grid<T>::fill(T element)
-{
-    assert(data != nullptr);
-
-    for (int i = 0; i < 16 * 16; i++)
-    {
-        data[i] = element;
-    }
-}
-
-template <typename T>
-T& Grid<T>::operator[](int index)
-{
-    assert(data != nullptr);
-    assert(index >= 0 && index < 16 * 16);
-
-    return data[index];
-}
-
-template <typename T>
-T& Grid<T>::operator()(int x, int y)
-{
-    assert(data != nullptr);
-    assert(x >= 0 && x < 16 * 16 && y >= 0 && y < 16 * 16);
-
-    return data[y * 16 + x];
-}
