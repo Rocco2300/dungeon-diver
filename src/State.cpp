0be4eb203d8b0ddfc4c0f97d20acc7e1928b1955
Rocco <grigoreandrey23@gmail.com>
2022-07-24 11:55:33 +0300

    implement state functions

--- src/State.cpp
+++ src/State.cpp
@@ -1,8 +1,24 @@
 #include "State.h"
 
+#include "StateStack.h"
+
 State::State(StateStack& stateStack, Context& context)
 {
     this->stateStack = &stateStack;
     this->context = &context;
 }
 
+void State::requestStackPush(StateID stateID)
+{
+    stateStack->pushState(stateID);
+}
+
+void State::requestStackClear()
+{
+    stateStack->clearStates();
+}
+
+void State::requestStackPop()
+{
+    stateStack->popState();
+}
