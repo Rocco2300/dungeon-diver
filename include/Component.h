506a916894254e2aba7624e00c3d8b1ca88cdebc
Rocco <grigoreandrey23@gmail.com>
2022-07-27 13:07:34 +0300

    add gui component header

---
+++ include/Component.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <memory>
+
+#include <SFML/Window/Event.hpp>
+
+namespace GUI
+{
+
+class Component
+{
+public:
+    using Ptr = std::shared_ptr<Component>;
+
+private: 
+    bool isActive;
+    bool isSelected;
+
+public:
+    Component() : isActive{false}, isSelected{false} { }
+    virtual ~Component() = default;
+
+    virtual bool isSelectable() const = 0;
+            bool isSelected() const;
+    virtual void select();
+    virtual void deselect();
+
+    virtual bool isActive() const;
+    virtual void activate();
+    virtual void deactivate();
+
+    virtual void handleEvent(const sf::Event& event) = 0;
+};
+
+};
