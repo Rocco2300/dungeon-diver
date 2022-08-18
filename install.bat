12eeeed36e67bec575e2113379244da910d89038
Rocco <grigoreandrey23@gmail.com>
2022-08-18 11:46:59 +0300

    update install bat

--- install.bat
+++ install.bat
@@ -1,12 +1,14 @@
-@mkdir obj
 @mkdir lib
 @mkdir build
+@mkdir lib\bin
+@mkdir lib\SFML
 @git clone --branch 2.5.1 https://github.com/SFML/SFML.git
 @cmake -G "MinGW Makefiles" -S SFML -B build
 @mingw32-make -C ./build
-@move build\lib\*.a lib
-@move build\lib\*.dll .
+@move build\lib\*.a lib\SFML
+@move build\lib\*.dll .\lib\bin
 @move SFML\include\SFML include
-@move SFML\extlibs\bin\x64\openal32.dll .
-@rd /s /q build
-@rd /s /q SFML
+@move SFML\extlibs\bin\x64\openal32.dll .\lib\bin
+@rd /q /s build
+@rd /q /s SFML
+@mkdir build
