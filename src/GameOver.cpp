52c1a28f4c3540e26fea558583305c7690a0077a
Rocco <grigoreandrey23@gmail.com>
2022-09-10 20:11:19 +0300

    fix game over label not showing

--- src/GameOver.cpp
+++ src/GameOver.cpp
@@ -4,8 +4,9 @@
 #include "Label.h"
 #include "StateStack.h"
 
-GameOver::GameOver(StateStack &stateStack, Context context)
-    : State(stateStack, context) {
+GameOver::GameOver(StateStack& stateStack, Context context)
+    : State(stateStack, context)
+{
     initUI();
 
     backgroundImage.create(WINDOW_SIZE, WINDOW_SIZE, sf::Color(0, 0, 0, 150));
@@ -16,7 +17,8 @@ GameOver::GameOver(StateStack &stateStack, Context context)
     backgroudSprite.setTexture(backgroundTexture);
 }
 
-void GameOver::initUI() {
+void GameOver::initUI()
+{
     auto gameOverLabel = std::make_shared<GUI::TextLabel>();
     gameOverLabel->setSize({9 * 3, 5});
     gameOverLabel->setPosition({2, 2});
@@ -29,35 +31,36 @@ void GameOver::initUI() {
 
     container.setSize({9 * 3 + 8 + 4, 9});
     container.setPosition({static_cast<float>(posX), static_cast<float>(posY)});
-    container.getBackgoundRef().setFillColor(sf::Color::Black);
-    container.getBackgoundRef().setOutlineColor(sf::Color::White);
-    container.getBackgoundRef().setOutlineThickness(1);
-    container.getBackgoundRef().setSize({9 * 3 + 8 + 4, 9});
+    container.setBackgroundColor(sf::Color::Black);
+    container.setOutlineColor(sf::Color::White);
+    container.setOutlineThickness(1);
 
     container.pack(gameOverLabel);
 }
 
-bool GameOver::handleEvent(const sf::Event &event) {
-    if (event.type == sf::Event::KeyPressed) {
-        switch (event.key.code) {
-            case sf::Keyboard::Escape:
-                stateStack->clearStates();
-                stateStack->pushState(StateID::MainMenu);
-            case sf::Keyboard::Enter:
-                stateStack->clearStates();
-                stateStack->pushState(StateID::Game);
-            default:
-                break;
+bool GameOver::handleEvent(const sf::Event& event)
+{
+    if (event.type == sf::Event::KeyPressed)
+    {
+        switch (event.key.code)
+        {
+        case sf::Keyboard::Escape:
+            stateStack->clearStates();
+            stateStack->pushState(StateID::MainMenu);
+        case sf::Keyboard::Enter:
+            stateStack->clearStates();
+            stateStack->pushState(StateID::Game);
+        default:
+            break;
         }
     }
     return true;
 }
 
-bool GameOver::update(sf::Time dt) {
-    return false;
-}
+bool GameOver::update(sf::Time dt) { return false; }
 
-void GameOver::draw() {
+void GameOver::draw()
+{
     context.texture->draw(backgroudSprite);
     context.texture->draw(container);
 }
