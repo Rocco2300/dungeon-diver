ae682cee95ab8479bc9ab48552d4fac480ec42ca
Rocco <grigoreandrey23@gmail.com>
2022-09-15 16:32:46 +0300

    add clamp function - should rename file :/

--- include/util/Lerp.h
+++ include/util/Lerp.h
@@ -2,7 +2,8 @@
 
 #include <SFML/Graphics.hpp>
 
-extern float lerp(float a, float b, float t);
+extern float        lerp(float a, float b, float t);
 extern sf::Vector2f lerp(sf::Vector2f a, sf::Vector2f b, float t);
 
+extern void  clamp(float& num, float a, float b);
 extern float distance(sf::Vector2f a, sf::Vector2f b);
--- src/util/Lerp.cpp
+++ src/util/Lerp.cpp
@@ -2,23 +2,22 @@
 
 #include <cmath>
 
-float lerp(float a, float b, float t)
-{
-    return a + (b - a) * t;
-}
+float lerp(float a, float b, float t) { return a + (b - a) * t; }
 
 sf::Vector2f lerp(sf::Vector2f a, sf::Vector2f b, float t)
 {
-    return {
-        a.x + (b.x - a.x) * t,
-        a.y + (b.y - a.y) * t
-    };
+    return {a.x + (b.x - a.x) * t, a.y + (b.y - a.y) * t};
+}
+
+void clamp(float& num, float a, float b)
+{
+    if (num < a)
+        num = a;
+    else if (num > b)
+        num = b;
 }
 
 float distance(sf::Vector2f a, sf::Vector2f b)
 {
-    return std::sqrt(
-        std::pow(b.x - a.x, 2) +
-        std::pow(b.y - a.y, 2)
-    );
+    return std::sqrt(std::pow(b.x - a.x, 2) + std::pow(b.y - a.y, 2));
 }
