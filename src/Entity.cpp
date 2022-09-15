e8b99034a3e474fe33a31d08cd186a62f4967a31
Rocco <grigoreandrey23@gmail.com>
2022-09-15 16:33:10 +0300

    clamp the flip variable to -1 to 1

--- src/Entity.cpp
+++ src/Entity.cpp
@@ -1,16 +1,17 @@
 
 #include "Entity.h"
 
-#include <iostream>
 #include <cmath>
+#include <iostream>
 
-#include "World.h"
 #include "AssetManager.h"
+#include "Lerp.h"
+#include "World.h"
 
 Entity::Entity()
 {
-    t = 1.f;
-    flip = 1.f;
+    t     = 1.f;
+    flip  = 1.f;
     frame = 0.f;
 
     pos.x = 0.f;
@@ -21,42 +22,24 @@ Entity::Entity()
     const std::string path = std::string(PROJ_PATH) + "shader/shader.frag";
     if (!shader.loadFromFile(path, sf::Shader::Fragment))
         std::cerr << "Error loading shader.";
-    
+
     tDmg = 0.f;
     shader.setUniform("percent", tDmg);
     shader.setUniform("texture", sf::Shader::CurrentTexture);
     shader.setUniform("flashColor", sf::Glsl::Vec4(1, 1, 1, 1));
 }
 
-void Entity::setWorld(World& world)
-{
-    this->world = &world;
-}
+void Entity::setWorld(World& world) { this->world = &world; }
 
-int Entity::getHp()
-{
-    return hp;
-}
+int Entity::getHp() { return hp; }
 
-int Entity::getDamage()
-{
-    return damage;
-}
+int Entity::getDamage() { return damage; }
 
-bool Entity::isDead()
-{
-    return dead;
-}
+bool Entity::isDead() { return dead; }
 
-void Entity::setHP(int hp)
-{
-    this->hp = hp;
-}
+void Entity::setHP(int hp) { this->hp = hp; }
 
-void Entity::setDamage(int damage)
-{
-    this->damage = damage;
-}
+void Entity::setDamage(int damage) { this->damage = damage; }
 
 void Entity::setSprite(std::string path, sf::Vector2i size)
 {
@@ -68,19 +51,14 @@ void Entity::takeDamage(int damageAmount)
     tDmg = 1.f;
     hp -= damageAmount;
 
-    if (hp <= 0)
-        dead = true;
+    if (hp <= 0) dead = true;
 }
 
-void Entity::setPosition(sf::Vector2i pos)
-{
-    this->pos = pos;
-}
+void Entity::setPosition(sf::Vector2i pos) { this->pos = pos; }
 
 void Entity::move(sf::Vector2i o)
 {
-    if (t != 1)
-        return;
+    if (t != 1) return;
 
     pos += o;
 
@@ -92,16 +70,18 @@ void Entity::move(sf::Vector2i o)
     t = 0;
 
     if (o.x != 0)
+    {
         flip = o.x;
+        clamp(flip, -1.f, 1.f);
+    }
 
     animation = std::bind(&Entity::animateMove, this);
 }
 
 void Entity::bump(sf::Vector2i o)
 {
-    if (t != 1)
-        return;
-        
+    if (t != 1) return;
+
     offS.x = o.x * 8;
     offS.y = o.y * 8;
 
@@ -110,7 +90,10 @@ void Entity::bump(sf::Vector2i o)
     t = 0;
 
     if (o.x != 0)
+    {
         flip = o.x;
+        clamp(flip, -1.f, 1.f);
+    }
 
     animation = std::bind(&Entity::animateBump, this);
 }
@@ -123,43 +106,31 @@ void Entity::update(sf::Time dt)
     {
         tDmg -= 0.08f;
 
-        if (tDmg < 0.f)
-            tDmg = 0.f;
+        if (tDmg < 0.f) tDmg = 0.f;
     }
 
-    if (dead)
-        return;
+    if (dead) return;
 
     frame += dt.asSeconds() * 5;
 
-    sprite.setSpriteIndex((int)std::floor(frame) % 3);
+    sprite.setSpriteIndex((int) std::floor(frame) % 3);
 
-    if (t != 1)
-    {
-        animate(dt.asSeconds() * 8);
-    }
+    if (t != 1) { animate(dt.asSeconds() * 8); }
 }
 
-bool Entity::notMoving()
-{
-    return (t == 1);
-}
+bool Entity::notMoving() { return (t == 1); }
 
-sf::Vector2i Entity::getPosition()
-{
-    return pos;
-}
+sf::Vector2i Entity::getPosition() { return pos; }
 
 void Entity::draw(sf::RenderTarget& target, sf::RenderStates states) const
 {
-    if (dead)
-        return;
-        
+    if (dead) return;
+
     sf::Transform transform;
     transform.translate(pos.x * 8 + off.x, pos.y * 8 + off.y);
     transform.scale({flip, 1.f}, {4.f, 4.f});
     states.transform = transform;
-    states.shader = &shader;
+    states.shader    = &shader;
 
     target.draw(sprite, states);
 }
